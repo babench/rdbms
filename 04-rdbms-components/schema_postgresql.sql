43a82ed0b59c72d183e2335816b4f3aab2e9c741
Dmitriy Shishmakov <dashishmakov@domclick.ru>
2019-06-28 10:54:29 +0300

    Add indexes with comments for hw-04

--- 04-rdbms-components/schema_postgresql.sql
+++ 04-rdbms-components/schema_postgresql.sql
@@ -31,6 +31,9 @@ COMMENT ON COLUMN otus.manufacturer.description IS 'manufacturer''s name or desc
 COMMENT ON COLUMN otus.manufacturer.created_time IS 'creation timestamp in DB; it is a timestamp with a time zone defines an exact moment when the data had appeared';
 COMMENT ON COLUMN otus.manufacturer.updated_time IS 'last updated timestamp; it is a timestamp with a time zone defines an exact moment when the data had updated';
 
+CREATE INDEX IF NOT EXISTS otus.manufacturer_tag_idx ON otus.manufacturer (tag);
+COMMENT ON INDEX otus.manufacturer_tag_idx IS 'tag is metadata for searching by manufacturer';
+
 
 CREATE TABLE IF NOT EXISTS otus.supplier
 (
@@ -42,10 +45,13 @@ CREATE TABLE IF NOT EXISTS otus.supplier
 );
 COMMENT ON TABLE otus.supplier IS 'companies responsible for the logistics';
 COMMENT ON COLUMN otus.supplier.id IS 'surrogate identifier; auto sequence of the big integer is a good choice for a long time e-commerce store';
-COMMENT ON COLUMN otus.manufacturer.tag IS 'manufacturer''s short name for searching';
+COMMENT ON COLUMN otus.supplier.tag IS 'supplier''s short name for searching';
 COMMENT ON COLUMN otus.supplier.description IS 'supplier''s name or description; varchar is a variable-length character type with a chance to set a limit size of data';
 COMMENT ON COLUMN otus.supplier.created_time IS 'creation timestamp in DB; it is a timestamp with a time zone defines an exact moment when the data had appeared';
 COMMENT ON COLUMN otus.supplier.updated_time IS 'last updated timestamp; it is a timestamp with a time zone defines an exact moment when the data had updated';
+
+CREATE INDEX IF NOT EXISTS otus.supplier_tag_idx ON otus.supplier (tag);
+COMMENT ON INDEX otus.supplier_tag_idx IS 'tag is metadata for searching by supplier';
 
 
 CREATE TABLE IF NOT EXISTS otus.product
@@ -71,25 +77,35 @@ COMMENT ON COLUMN otus.product.deleted IS 'product accessibility flag; true and 
 COMMENT ON COLUMN otus.product.created_time IS 'creation timestamp in DB; it is a timestamp with a time zone defines an exact moment when the data had appeared';
 COMMENT ON COLUMN otus.product.updated_time IS 'last updated timestamp; it is a timestamp with a time zone defines an exact moment when the data had updated';
 
+CREATE INDEX IF NOT EXISTS otus.product_manufacturer_id_supplier_id_idx ON otus.product (manufacturer_id, supplier_id);
+COMMENT ON INDEX otus.product_manufacturer_id_supplier_id_idx IS 'need to search for products by manufacturers and suppliers';
+CREATE INDEX IF NOT EXISTS otus.product_tag_idx ON otus.product (tag);
+COMMENT ON INDEX otus.product_tag_idx IS 'tag is metadata for searching by supplier';
+CREATE INDEX IF NOT EXISTS otus.product_deleted_idx ON otus.product (deleted, count) where deleted = false and count > 0;
+COMMENT ON INDEX otus.product_deleted_idx IS 'search for products currently available for purchases';
+
 
 CREATE TABLE IF NOT EXISTS otus.product_property
 (
-  id           BIGSERIAL PRIMARY KEY,
-  product_id   BIGINT        NOT NULL REFERENCES otus.product (id),
-  property     VARCHAR(255)  NOT NULL,
-  description  VARCHAR(1024) NOT NULL,
-  comment      VARCHAR(1024),
-  created_time TIMESTAMPTZ   NOT NULL DEFAULT now(),
-  updated_time TIMESTAMPTZ
+  id            BIGSERIAL PRIMARY KEY,
+  product_id    BIGINT        NOT NULL REFERENCES otus.product (id),
+  property_name VARCHAR(255)  NOT NULL,
+  property_desc VARCHAR(1024) NOT NULL,
+  comment       VARCHAR(1024),
+  created_time  TIMESTAMPTZ   NOT NULL DEFAULT now(),
+  updated_time  TIMESTAMPTZ
 );
 COMMENT ON TABLE otus.product_property IS 'properties for each product';
 COMMENT ON COLUMN otus.product_property.id IS 'surrogate identifier; auto sequence of the big integer is a good choice for a long time e-commerce store';
 COMMENT ON COLUMN otus.product_property.product_id IS 'product identifier (FK)';
-COMMENT ON COLUMN otus.product_property.property IS 'name of product property; varchar is a variable-length character type with a chance to set a limit size of data';
-COMMENT ON COLUMN otus.product_property.description IS 'description of product property; varchar is a variable-length character type with a chance to set a limit size of data';
+COMMENT ON COLUMN otus.product_property.property_name IS 'name of product property; varchar is a variable-length character type with a chance to set a limit size of data';
+COMMENT ON COLUMN otus.product_property.property_desc IS 'description of product property; varchar is a variable-length character type with a chance to set a limit size of data';
 COMMENT ON COLUMN otus.product_property.comment IS 'common comment; varchar is a variable-length character type with a chance to set a limit size of data';
 COMMENT ON COLUMN otus.product_property.created_time IS 'creation timestamp in DB; it is a timestamp with a time zone defines an exact moment when the data had appeared';
 COMMENT ON COLUMN otus.product_property.updated_time IS 'last updated timestamp; it is a timestamp with a time zone defines an exact moment when the data had updated';
+
+CREATE INDEX IF NOT EXISTS otus.product_product_id_idx ON otus.product_property (product_id);
+COMMENT ON INDEX otus.product_product_id_idx IS 'helps to select exact properties for the products';
 
 
 CREATE TABLE IF NOT EXISTS otus.product_price
@@ -108,12 +124,17 @@ COMMENT ON COLUMN otus.product_price.product_id IS 'product identifier (FK)';
 COMMENT ON COLUMN otus.product_price.supplier_id IS 'supplier identifier (FK)';
 COMMENT ON COLUMN otus.product_price.manufacturer_id IS 'manufacturer identifier (FK)';
 
+CREATE INDEX IF NOT EXISTS otus.product_price_price_idx ON otus.product_price (price);
+COMMENT ON INDEX otus.product_price_price_idx IS 'helps to get ranges of product prices';
+CREATE INDEX IF NOT EXISTS otus.product_price_product_id_idx ON otus.product_price (product_id);
+COMMENT ON INDEX otus.product_price_product_id_idx IS 'get product price';
+
 
 CREATE TABLE IF NOT EXISTS otus.account
 (
   id           BIGSERIAL PRIMARY KEY,
   pwd_hash     VARCHAR(255)      NOT NULL,
-  email        VARCHAR(50)       NOT NULL UNIQUE,
+  email        VARCHAR(50)       NOT NULL,
   phone        VARCHAR(15),
   type         otus.account_type NOT NULL,
   first_name   VARCHAR(100),
@@ -138,6 +159,11 @@ COMMENT ON COLUMN otus.account.created_time IS 'creation timestamp in DB; it is 
 COMMENT ON COLUMN otus.account.updated_time IS 'last updated timestamp; it is a timestamp with a time zone defines an exact moment when the data had updated';
 COMMENT ON COLUMN otus.account.birthdate IS 'account birthdate; only date in the year';
 
+CREATE UNIQUE INDEX IF NOT EXISTS otus.account_email_idx ON otus.account (email);
+COMMENT ON INDEX otus.account_email_idx IS 'e-mail should be unique for all accounts; this is user login';
+CREATE INDEX IF NOT EXISTS otus.account_deleted_type_idx ON otus.account (deleted, type) where deleted = false;
+COMMENT ON INDEX otus.account_deleted_type_idx IS 'search among available accounts';
+
 
 CREATE TABLE IF NOT EXISTS otus.order
 (
@@ -147,8 +173,7 @@ CREATE TABLE IF NOT EXISTS otus.order
   status         otus.order_status NOT NULL,
   created_time   TIMESTAMPTZ       NOT NULL DEFAULT now(),
   scheduled_time TIMESTAMPTZ       NOT NULL,
-  delivered_time TIMESTAMPTZ,
-  UNIQUE (owner_id, product_id, status, created_time)
+  delivered_time TIMESTAMPTZ
 );
 COMMENT ON TABLE otus.order IS 'clients orders';
 COMMENT ON COLUMN otus.order.id IS 'surrogate identifier; auto sequence of the big integer is a good choice for a long time e-commerce store';
@@ -158,6 +183,11 @@ COMMENT ON COLUMN otus.order.status IS 'order status; enum type comprises a stat
 COMMENT ON COLUMN otus.order.created_time IS 'creation timestamp in DB; it is a timestamp with a time zone defines an exact moment when the data had appeared';
 COMMENT ON COLUMN otus.order.scheduled_time IS 'scheduled delivery date and time; helps to define a date and time in a concrete time zone';
 COMMENT ON COLUMN otus.order.delivered_time IS 'actual delivery date and time; helps to define a date and time in a concrete time zone';
+
+CREATE INDEX IF NOT EXISTS otus.oder_product_id_owner_id_status_idx ON otus.order (product_id, owner_id, status);
+COMMENT ON INDEX otus.oder_product_id_owner_id_status_idx IS 'filter orders by product, buyer and order status';
+CREATE INDEX IF NOT EXISTS otus.oder_created_time_scheduled_time_idx ON otus.order (created_time, scheduled_time);
+COMMENT ON INDEX otus.oder_created_time_scheduled_time_idx IS 'sort and filter orders by creation time';
 
 
 CREATE TABLE IF NOT EXISTS otus.order_details
@@ -183,6 +213,9 @@ COMMENT ON COLUMN otus.order_details.total_price IS 'final price after calculati
 COMMENT ON COLUMN otus.order_details.created_time IS 'creation timestamp in DB; it is a timestamp with a time zone defines an exact moment when the data had appeared';
 COMMENT ON COLUMN otus.order_details.updated_time IS 'last updated timestamp; it is a timestamp with a time zone defines an exact moment when the data had updated';
 
+CREATE INDEX IF NOT EXISTS otus.order_details_order_id_idx ON otus.order_details (order_id);
+COMMENT ON INDEX otus.order_details_order_id_idx IS 'select detail info for order';
+
 
 CREATE TABLE IF NOT EXISTS otus.order_log
 (
@@ -198,3 +231,6 @@ COMMENT ON COLUMN otus.order_log.order_id IS 'oder identifier (FK)';
 COMMENT ON COLUMN otus.order_log.modified_by IS 'account identifier changed the order status (FK)';
 COMMENT ON COLUMN otus.order_log.status IS 'order status; enum type comprises a static and ordered set of values that helps to escape errors';
 COMMENT ON COLUMN otus.order_log.created_time IS 'creation timestamp in DB; it is a timestamp with a time zone defines an exact moment when the data had appeared';
+
+CREATE INDEX IF NOT EXISTS otus.order_log_order_id_idx ON otus.order_log (order_id);
+COMMENT ON INDEX otus.order_log_order_id_idx IS 'select log records for order';
